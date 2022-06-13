4b3f0bedd29270b26ae19673349032e9a454b870
John <gopal1409@gmail.com>
2022-06-13 14:40:53 +0530

    terraform with terraform cloud

--- c1-version.tf
+++ c1-version.tf
@@ -4,13 +4,19 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      
+
     }
+    }
+    backend "remote" {
+      organization = "cli-driven-workflow-gd"
+      workspaces {
+        name = "cli-driven-azure-demo"
+      }
   }
 }
 
 # Provider Block
 provider "azurerm" {
- features {}          
+  features {}
 }
 
